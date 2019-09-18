b9182e80a6fee2ef4db994bfcc83c7c078d586ae
Verta-master <vertyagina@gmail.com>
2019-09-18 11:51:20 +0600

    Update 2019-09-18T05:51:15.235Z

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-jQuery(function($) {
-	"use strict";
-
 //Mobile menu
 $('.menu__btn').click(function() {
   $('.menu').toggleClass('menu--opened');
@@ -30,41 +27,3 @@ $(".btn-top").click(function () {
   return false;
 });
 
-//Scroll to menu anchor
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
-
-  //smoothscroll
-  $('.menu__item a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $('.menu__item a').each(function () {
-        $(this).removeClass('menu__link--active');
-    })
-    $(this).addClass('menu__link--active');
-
-    var target = this.hash,
-        menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 500, 'swing', function () {
-        $(document).on("scroll", onScroll);
-    });
-  });
-});
-
-function onScroll(event){
-  var scrollPos = $(document).scrollTop();
-  $('.menu__link').each(function () {
-      event.preventDefault();
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('.menu__link').removeClass("menu__link--active");
-          currLink.addClass("menu__link--active");
-      }
-  });
-};
-});
